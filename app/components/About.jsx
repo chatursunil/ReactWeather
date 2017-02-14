b230b3838ed7196fa8f4dab727e7aab16f97ade9
Sunil Chaturvedi <Sunil Chaturvedi>
2017-02-14 11:05:18 -0500

    Style About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,24 @@ const React = require('react');
 const About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to About Page</p>
+            <h1 className="text-center">About</h1>
+            <p>
+                This is Weather Application that has been developed using React.
+                I've built this app as part of the React course by Andrew Mead.
+            </p>
+            <p>
+                Here are some of tools that were used:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react" target="_blank">React</a> 
+                    - This is the javascript framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> 
+                    - I used this api to fetch the weather data
+                </li>
+            </ul>
         </div>
     );
 }
